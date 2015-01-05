107d20c13af3626e4914558f39b293f796f8899f
Andrea Falzetti <afalzettidroid@gmail.com>
2015-01-05 15:07:38 +0000

    First import

---
+++ test-page.php
@@ -0,0 +1,41 @@
+<?php include('compiler.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title><?php echo $config->title; ?></title>
+
+    <!-- Core CSS -->
+    <link href="style.css" rel="stylesheet">
+
+
+  </head>
+
+  <body>
+
+	<!-- Remove this section
+    ================================================== -->
+	<p></p>
+    <div class="container">
+        <div class="row">
+			<div class="col-lg-12">
+				<p>Test Page</p>
+			</div>
+        </div> <!-- .row -->   
+    </div><!-- .container -->
+
+	
+	
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
